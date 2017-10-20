eecbd81107b11004258e35f3690c81ba874d22e7
O'Bryan Verheyde <verheyde.obryan@gmail.com>
2017-10-20 10:12:19 +0200

    add data json

---
+++ data.js
@@ -0,0 +1,20 @@
+exports.movies = [
+  {
+    id: 1,
+    title: 'Star Wars, épisode VIII : Les Derniers Jedi',
+    description: 'film américain de science-fiction de type space opera',
+    imageURL: 'http://fr.web.img6.acsta.net/c_215_290/pictures/17/10/10/04/58/2061488.jpg'
+  },
+  {
+    id: 2,
+    title: 'Star Trek',
+    description: 'film américain de science-fiction de type space opera',
+    imageURL: 'http://www.startrek.com/uploads/assets/articles/9a5570aa205c967c350c52e4ad43bc8ab6fdecd0.png'
+  },
+  {
+    id: 3,
+    title: 'Star Wars, épisode VIII : Les Derniers Jedi',
+    description: 'film américain de science-fiction de type space opera',
+    imageURL: 'http://fr.web.img6.acsta.net/c_215_290/pictures/17/10/10/04/58/2061488.jpg'
+  }
+];
--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const app = express();
+const movies = require('./data.js');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.get('/movies', function(request, response) {
+  response.json(movies);
+});
+
+app.listen(3000);
